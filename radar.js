165a108bda8736b35befa4b583bf538ca22a48b3
maria <95863647+mariazelenika@users.noreply.github.com>
2023-01-27 19:00:44 +0100

    Create radar.js

---
+++ radar.js
@@ -0,0 +1,54 @@
+// Initialize the echarts instance based on the prepared dom
+var myChart = echarts.init(document.getElementById('main3'));
+
+var values = [4, 4, 5, 7, 7, 4, 5, 5, 6, 8, 7, 5];
+
+option = {
+  title: {
+    //text: 'Basic Radar Chart'
+  },
+  tooltip: {
+    trigger: 'item'
+  },
+  legend: {
+    data: ['Personal rating of the various skills', 'Actual Spending']
+  },
+  radar: {
+    // shape: 'circle',
+    indicator: [
+      { name: 'InfoViz', max: 10 },
+      { name: 'Stats', max: 10 },
+      { name: 'Math', max: 10 },
+      { name: 'Art', max: 10 },
+      { name: 'Computer', max: 10 },
+      { name: 'Code', max: 10 },
+      { name: 'Computer', max: 10 },
+      { name: 'CG Code', max: 10 },
+      { name: 'UX', max: 10 },
+      { name: 'Communication', max: 10 },
+      { name: 'Collaboration', max: 10 },
+      { name: 'Repository', max: 10 },
+    ]
+  },
+  series: [
+    {
+      name: 'Personal rating of the various skills',
+      type: 'radar',
+      data: [
+        {
+          value: values,
+          name: 'Skills',
+          areaStyle: {
+            color: 'rgba(84,112,198, 0.8)',
+          },
+          label: {
+            show: true
+          }
+        }
+      ]
+    }
+  ]
+};
+
+// Display the chart using the configuration items and data just specified.
+myChart.setOption(option);
