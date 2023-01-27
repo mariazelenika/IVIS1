3065018adffc2b3f32a0122f7e1b11caa2f46595
maria <95863647+mariazelenika@users.noreply.github.com>
2023-01-27 21:02:56 +0100

    Update radar.js

--- radar.js
+++ radar.js
@@ -1,57 +1,59 @@
-// Initialize the echarts instance based on the prepared dom
-var Chart = echarts.init(document.getElementById('main3'));
+export function draw(v) {
+    // Initialize the echarts instance based on the prepared dom
+    var Chart = echarts.init(document.getElementById('main3'));
 
-var values = [4, 4, 5, 7, 7, 4, 5, 5, 6, 8, 7, 5];
+    var values = v;
 
-option = {
-  title: {
-    //text: 'Basic Radar Chart'
-  },
-  grid: {
-    containLabel: true
-  },
-  tooltip: {
-    trigger: 'item'
-  },
-  legend: {
-    data: ['Personal rating of the various skills', 'Actual Spending']
-  },
-  radar: {
-    // shape: 'circle',
-    indicator: [
-      { name: 'InfoViz', max: 10 },
-      { name: 'Stats', max: 10 },
-      { name: 'Math', max: 10 },
-      { name: 'Art', max: 10 },
-      { name: 'Computer', max: 10 },
-      { name: 'Code', max: 10 },
-      { name: 'Computer', max: 10 },
-      { name: 'CG Code', max: 10 },
-      { name: 'UX', max: 10 },
-      { name: 'Communication', max: 10 },
-      { name: 'Collaboration', max: 10 },
-      { name: 'Repository', max: 10 },
-    ]
-  },
-  series: [
-    {
-      name: 'Personal rating of the various skills',
-      type: 'radar',
-      data: [
+    option = {
+      title: {
+        //text: 'Basic Radar Chart'
+      },
+      grid: {
+        containLabel: true
+      },
+      tooltip: {
+        trigger: 'item'
+      },
+      legend: {
+        data: ['Personal rating of the various skills', 'Actual Spending']
+      },
+      radar: {
+        // shape: 'circle',
+        indicator: [
+          { name: 'InfoViz', max: 10 },
+          { name: 'Stats', max: 10 },
+          { name: 'Math', max: 10 },
+          { name: 'Art', max: 10 },
+          { name: 'Computer', max: 10 },
+          { name: 'Code', max: 10 },
+          { name: 'Computer', max: 10 },
+          { name: 'CG Code', max: 10 },
+          { name: 'UX', max: 10 },
+          { name: 'Communication', max: 10 },
+          { name: 'Collaboration', max: 10 },
+          { name: 'Repository', max: 10 },
+        ]
+      },
+      series: [
         {
-          value: values,
-          name: 'Skills',
-          areaStyle: {
-            color: 'rgba(84,112,198, 0.8)',
-          },
-          label: {
-            show: true
-          }
+          name: 'Personal rating of the various skills',
+          type: 'radar',
+          data: [
+            {
+              value: values,
+              name: 'Skills',
+              areaStyle: {
+                color: 'rgba(84,112,198, 0.8)',
+              },
+              label: {
+                show: true
+              }
+            }
+          ]
         }
       ]
-    }
-  ]
-};
+    };
 
-// Display the chart using the configuration items and data just specified.
-Chart.setOption(option);
+    // Display the chart using the configuration items and data just specified.
+    Chart.setOption(option);
+}
