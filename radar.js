bc388fe879c96cd8ba7b04771d9198578a227adb
maria <95863647+mariazelenika@users.noreply.github.com>
2023-01-27 23:46:22 +0100

    Update radar.js

--- radar.js
+++ radar.js
@@ -16,7 +16,7 @@ export function draw(v) {
         trigger: 'item'
       },
       legend: {
-        left: 150,
+        left: '30%',
         data: ['Personal rating of the various skills']
       },
       radar: {
