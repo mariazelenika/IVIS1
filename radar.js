cc3f4726352f780e3e9a209c750a6031c5e206a4
maria <95863647+mariazelenika@users.noreply.github.com>
2023-01-29 16:24:32 +0100

    Update radar.js

--- radar.js
+++ radar.js
@@ -14,7 +14,7 @@ export function draw(values, max2, name) {
         trigger: 'item'
       },
       legend: {
-        left: '10%', 
+        left: '20%', 
         data: ['Combined skills of the group**']
       },
       radar: {
